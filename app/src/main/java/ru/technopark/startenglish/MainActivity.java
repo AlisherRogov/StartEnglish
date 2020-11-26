0e7ef7b6961fde11c2b46f8a554221cbf89af3f2
AlisherRogov <rogov.alisher@gmail.com>
2020-11-26 11:30:00 +0300

    executorService added

--- app/src/main/java/ru/technopark/startenglish/MainActivity.java
+++ app/src/main/java/ru/technopark/startenglish/MainActivity.java
@@ -1,28 +1,8 @@
 package ru.technopark.startenglish;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ScrollView;
-import android.widget.TextView;
-
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import ru.technopark.startenglish.network.ApiRepo;
-import ru.technopark.startenglish.network.DictionaryApi;
-import ru.technopark.startenglish.word.Word;
-import ru.technopark.startenglish.word.WordViewModel;
-
 
 public class MainActivity extends AppCompatActivity {
 
